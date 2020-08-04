9e6a8cc9cacda679289a57d3b1133c29545a65de
cloudsystems22 <cloudsystems22@gmail.com>
2020-08-04 09:30:03 -0300

    Ultima atualização nodemon

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Bem vindo a uma aplicação node.js")
+    res.send("Bem vindo a uma aplicação node.js, testando Elastic Beanstalk AWS!")
 });
 
 
